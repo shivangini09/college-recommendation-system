afcf9fb9845ee95e659168919d985ff035544d8b
shivangini09 <b22ph012@iitj.ac.in>
2025-06-04 17:33:09 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,4 +14,4 @@ app.use('/api/colleges', collegeRoutes);
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
-app.post('/api/colleges/generate-preference', collegeController.generatePreferenceList);
+// app.post('/api/colleges/generate-preference', collegeController.generatePreferenceList);
