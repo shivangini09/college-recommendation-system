5a5d4803f68bed4e3a3caead438cd839faefed9b
shivangini09 <b22ph012@iitj.ac.in>
2025-09-07 23:12:14 +0530

    update colleges.js

--- backend/routes/colleges.js
+++ backend/routes/colleges.js
@@ -1,16 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const collegeController = require('../controllers/collegeController');
-
-// // Route for SearchForm
-// router.post('/recommend', collegeController.recommendColleges);
-
-// // ✅ New route for PreferenceForm
-// router.post('/generate-preference', collegeController.generatePreferenceList);
-
-// module.exports = router;
-
-
 const express = require('express');
 const router = express.Router();
 const collegeController = require('../controllers/collegeController');
